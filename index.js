29b5be59d93c26599f6c7bfef0b6362dc12f1ae1
Magnus Hoff <magnus.hoff@brik.no>
2013-05-16 19:29:56 +0200

    Made the webui into a module

--- index.js
+++ index.js
@@ -2,67 +2,22 @@
 
 var fs = require('fs');
 var express = require('express');
-var mu = require("mu2");
 var optimist = require("optimist");
 var logginator = require("logginator");
-
-mu.root = __dirname + "/templates";
-
 var SubprocessManager = require("./model/subprocess-manager");
 var ActionManager = require("./model/action-manager");
-var views = {
-    subprocess: require("./view/subprocess"),
-    index: require("./view/index")
-};
-
+var webui = require("./webui");
 
 var log = logginator();
-
 
 var actionManager = new ActionManager(JSON.parse(fs.readFileSync("actions.json", "utf8")));
 var subprocessManager = new SubprocessManager(log.createSublogger("subprocessManager"));
 
-
 var app = express();
 require('winston-tagged-http-logger')(app, log.createSublogger("http"));
 
-app.use("/static", express.static(__dirname + "/static"));
 
-app.get('/', function (req, res, next) {
-    views.index(actionManager, subprocessManager, res);
-});
-
-app.get(/\/subprocess\/(\d+)$/, function (req, res, next) {
-    var id = parseInt(req.params[0], 10);
-
-    var subprocess = subprocessManager.get(id);
-    if (!subprocess) {
-       res.send(404);
-       return;
-    }
-
-    views.subprocess(subprocess, res);
-});
-
-app.post(/\/action\/([^\/]+)$/, function (req, res, next) {
-    var id = req.params[0];
-
-    var action = actionManager.get(id);
-    if (!action) {
-       res.send(404);
-       return;
-    }
-
-    var contentType = req.headers["content-type"];
-    if (contentType === "application/vnd.brik.roat.trigger+json") {
-        var id = action.trigger(subprocessManager);
-        res.setHeader("Location", "/subprocess/" + id);
-        res.send(201, "Created\n");
-    } else {
-        res.setHeader("Accept", "application/vnd.brik.roat.trigger+json");
-        res.send(415, "415 Unsupported Media Type\n");
-    }
-});
+webui(actionManager, subprocessManager, app);
 
 
 var argv = optimist.default({
---
+++ webui/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var mu = require("mu2");
+mu.root = __dirname + "/templates";
+
+var views = {
+    subprocess: require("./view/subprocess"),
+    index: require("./view/index")
+};
+
+module.exports = function (actionManager, subprocessManager, expressApp) {
+	expressApp.use("/static", express.static(__dirname + "/static"));
+
+	expressApp.get('/', function (req, res, next) {
+	    views.index(actionManager, subprocessManager, res);
+	});
+
+	expressApp.get(/\/subprocess\/(\d+)$/, function (req, res, next) {
+	    var id = parseInt(req.params[0], 10);
+
+	    var subprocess = subprocessManager.get(id);
+	    if (!subprocess) {
+	       res.send(404);
+	       return;
+	    }
+
+	    views.subprocess(subprocess, res);
+	});
+
+	expressApp.post(/\/action\/([^\/]+)$/, function (req, res, next) {
+	    var id = req.params[0];
+
+	    var action = actionManager.get(id);
+	    if (!action) {
+	       res.send(404);
+	       return;
+	    }
+
+	    var contentType = req.headers["content-type"];
+	    if (contentType === "application/vnd.brik.roat.trigger+json") {
+	        var id = action.trigger(subprocessManager);
+	        res.setHeader("Location", "/subprocess/" + id);
+	        res.send(201, "Created\n");
+	    } else {
+	        res.setHeader("Accept", "application/vnd.brik.roat.trigger+json");
+	        res.send(415, "415 Unsupported Media Type\n");
+	    }
+	});
+};
