f4c8b1e17a32b1a1ab3ab8fc7b88007aa6b4bff4
Magnus Hoff <magnus.hoff@brik.no>
2013-05-21 12:46:28 +0200

    Converted indentation to spaces

--- webui/index.js
+++ webui/index.js
@@ -8,41 +8,41 @@ var views = {
 };
 
 module.exports = function (actionManager, subprocessManager, expressApp) {
-	expressApp.use("/static", express.static(__dirname + "/static"));
+    expressApp.use("/static", express.static(__dirname + "/static"));
 
-	expressApp.get('/', function (req, res, next) {
-	    views.index(actionManager, subprocessManager, res);
-	});
+    expressApp.get('/', function (req, res, next) {
+        views.index(actionManager, subprocessManager, res);
+    });
 
-	expressApp.get(/\/subprocess\/(\d+)$/, function (req, res, next) {
-	    var id = parseInt(req.params[0], 10);
+    expressApp.get(/\/subprocess\/(\d+)$/, function (req, res, next) {
+        var id = parseInt(req.params[0], 10);
 
-	    var subprocess = subprocessManager.get(id);
-	    if (!subprocess) {
-	       res.send(404);
-	       return;
-	    }
+        var subprocess = subprocessManager.get(id);
+        if (!subprocess) {
+           res.send(404);
+           return;
+        }
 
-	    views.subprocess(subprocess, res);
-	});
+        views.subprocess(subprocess, res);
+    });
 
-	expressApp.post(/\/action\/([^\/]+)$/, function (req, res, next) {
-	    var id = req.params[0];
+    expressApp.post(/\/action\/([^\/]+)$/, function (req, res, next) {
+        var id = req.params[0];
 
-	    var action = actionManager.get(id);
-	    if (!action) {
-	       res.send(404);
-	       return;
-	    }
+        var action = actionManager.get(id);
+        if (!action) {
+           res.send(404);
+           return;
+        }
 
-	    var contentType = req.headers["content-type"];
-	    if (contentType === "application/vnd.brik.roat.trigger+json") {
-	        var id = action.trigger(subprocessManager);
-	        res.setHeader("Location", "/subprocess/" + id);
-	        res.send(201, "Created\n");
-	    } else {
-	        res.setHeader("Accept", "application/vnd.brik.roat.trigger+json");
-	        res.send(415, "415 Unsupported Media Type\n");
-	    }
-	});
+        var contentType = req.headers["content-type"];
+        if (contentType === "application/vnd.brik.roat.trigger+json") {
+            var id = action.trigger(subprocessManager);
+            res.setHeader("Location", "/subprocess/" + id);
+            res.send(201, "Created\n");
+        } else {
+            res.setHeader("Accept", "application/vnd.brik.roat.trigger+json");
+            res.send(415, "415 Unsupported Media Type\n");
+        }
+    });
 };
