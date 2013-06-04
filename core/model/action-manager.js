97aca456e9c139cef42011f0234399bc16867f59
Magnus Hoff <magnus.hoff@brik.no>
2013-06-04 13:55:28 +0200

    Whitespace (indent with spaces)

--- core/model/action-manager.js
+++ core/model/action-manager.js
@@ -2,29 +2,29 @@ var DaemonAction = require('./daemon-action');
 var ImmediateAction = require('./action');
 
 function ActionManager(spec) {
-	this.actionList = [];
-	this.actionDict = {};
+    this.actionList = [];
+    this.actionDict = {};
 
-	for (var id in spec) {
-		if (!spec.hasOwnProperty(id)) continue;
-		var actionSpec = spec[id];
+    for (var id in spec) {
+        if (!spec.hasOwnProperty(id)) continue;
+        var actionSpec = spec[id];
 
-		var constructor = ImmediateAction;
-		if (actionSpec.mode === 'daemon') {
-			constructor = DaemonAction;
-		}
-		this.push(new constructor(id, actionSpec));
-	}
+        var constructor = ImmediateAction;
+        if (actionSpec.mode === 'daemon') {
+            constructor = DaemonAction;
+        }
+        this.push(new constructor(id, actionSpec));
+    }
 }
 
 ActionManager.prototype.push = function (action) {
-	this.actionDict[action.id] = action;
-	this.actionList.push(action);
+    this.actionDict[action.id] = action;
+    this.actionList.push(action);
 };
 
 ActionManager.prototype.get = function (id) {
-	if (!this.actionDict.hasOwnProperty(id)) return undefined;
-	return this.actionDict[id];
+    if (!this.actionDict.hasOwnProperty(id)) return undefined;
+    return this.actionDict[id];
 };
 
 module.exports = ActionManager;
--- core/model/action.js
+++ core/model/action.js
@@ -1,12 +1,12 @@
 function Action(id, spec) {
-	this.id = id;
-	this.title = spec.title;
-	this.cmd = spec.cmd;
-	this.opts = spec.opts;
+    this.id = id;
+    this.title = spec.title;
+    this.cmd = spec.cmd;
+    this.opts = spec.opts;
 }
 
 Action.prototype.trigger = function (log, subprocessManager) {
-	return subprocessManager.execute(this.title, this.cmd, this.opts);
+    return subprocessManager.execute(this.title, this.cmd, this.opts);
 };
 
 module.exports = Action;
