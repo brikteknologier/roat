91decdedfe9f827fb6092b5e5afc91037253b5b5
Magnus Hoff <magnus.hoff@brik.no>
2013-06-06 12:30:28 +0200

    Implemented autostart feature to make restarting roat less disruptive

---
+++ subsystems/autostart.js
@@ -0,0 +1,25 @@
+module.exports = function (log, app, expressApp, configArgument) {
+    var delay = 1000;
+    var scheduled = [];
+
+    configArgument.forEach(function (id) {
+        var action = app.actionManager.get(id);
+        if (!action) {
+            log.error("Unable to find action with id " + JSON.stringify(id));
+            return;
+        }
+
+        var actionLog = log.createSublogger(id);
+        setTimeout(function () {
+            log.info("Triggering " + JSON.stringify(id));
+            var subprocessId = action.trigger(actionLog, app.subprocessManager);
+        }, delay);
+        delay += 1000;
+
+        scheduled.push(id);
+    });
+
+    if (scheduled.length) {
+        log.info("Scheduled the following actions for automatic execution: " + JSON.stringify(scheduled));
+    }
+};
--- subsystems/index.js
+++ subsystems/index.js
@@ -1,4 +1,5 @@
 var subsystemNames = [
+	'autostart',
 	'github',
 	'webui'
 ];
